b10350517291107dfbb696cbb6dbd63eac27f61f
tpe <Tom.Peetz@opitz-consulting.de>
2014-04-15 13:51:08 +0200

    Function.java zu static gemacht

--- function/Function.java
+++ function/Function.java
@@ -7,14 +7,14 @@ import parser.VPObject;
 
 public class Function {
 	
-	public String readFunctionFromStdin(){
+	public static String readFunctionFromStdin(){
 		Scanner in = new Scanner(System.in);
 		String function = in.next();
 		in.close();
 		return function;
 	}
 	
-	public String derivationFunction(ArrayList<VPObject> vpobjects, ArrayList<String> operations){
+	public static String derivationFunction(ArrayList<VPObject> vpobjects, ArrayList<String> operations){
 		String diff = "";
 		for(VPObject vpo : vpobjects){
 			diff += operations.get(vpobjects.indexOf(vpo)) + vpo.variableDiff() + "x" + "^" + vpo.potencyDiff();
@@ -22,7 +22,7 @@ public class Function {
 		return diff;
 	}
 	
-	public void calculateDerivation(VPObject vpobject){
+	public static void calculateDerivation(VPObject vpobject){
 		double variable = Double.parseDouble(vpobject.variable());
 		int potency = Integer.parseInt(vpobject.potency());
 		double variableDiff = variable * potency;
@@ -31,7 +31,10 @@ public class Function {
 		vpobject.setPotencyDiff(String.valueOf(potencyDiff));
 	}
 	
-	public double functionValue(String function, double x){
+	public static double functionValue(String function, double x){
+		
+		
+		
 		return 0.0; //Todo!
 	}
 
