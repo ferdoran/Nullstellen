32fe1288823828a4edba73a8b48fbd6c9f606340
tpe <Tom.Peetz@opitz-consulting.de>
2014-04-22 10:55:40 +0200

    Main aktualisiert und Ableitung.java wie in master geloescht.

--- NullstellenMain.java
+++ NullstellenMain.java
@@ -1,58 +1,27 @@
-import java.util.ArrayList;
-
-import scanner.RegexScanner;
-import parser.VPObject;
-import parser.ComponentsManager;
 import function.Function;
 
 public class NullstellenMain {
 
 	public static void main(String[] args) {
-		//Funktion einlesen
-		Function function = new Function();
-		String functionString = function.readFunctionFromStdin();
-		
-		//Funktion => Scanner
-		RegexScanner scanner = new RegexScanner();
-		ArrayList<String> matches = null;
+		Function function = null;
 		try{
-			matches = scanner.matchFunction(functionString);
+			/*
+			 * z.B. x^2+2x
+			 * 		2x-1
+			 * 		+3x
+			 */
+			function = new Function(Function.readFunctionFromStdin());
 		}
 		catch(IllegalArgumentException e){
 			System.out.println(e.getMessage());
 			System.exit(1);
 		}
-		finally{
-			scanner = null;	
-		}
+		System.out.println(function.toString());
+		System.out.println(function.value(2));
 		
-		//Scanner matches => VPObject + Operationen
-		ComponentsManager vpManager = null;
-		try{
-			vpManager = new ComponentsManager(matches);
-		}
-		catch(IllegalArgumentException e){
-			System.out.println(e.getMessage());
-			System.exit(1);
-		}
-		matches = null;
-		
-		//Ergebnisse sichern
-		ArrayList<String> operations = vpManager.operations();
-		ArrayList<VPObject> vpobjects = vpManager.vpobjects();
-		
-		//Ableitung erstellen
-		for(VPObject vpo : vpobjects){
-			function.calculateDerivation(vpo);
-		}
-		String diff = function.derivationFunction(vpobjects, operations);
-		
-		//CleanUp
-		vpobjects = null;
-		operations = null;
-		
-		System.out.println(function);
-		System.out.println(diff);
+		Function derivation = function.derivation();
+		System.out.println(derivation.toString());
+		System.out.println(derivation.value(2));
 	}
 
 }
