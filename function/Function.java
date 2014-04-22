c3f3457f5db185cc93c51abde88fdb3e86a28912
tpe <Tom.Peetz@opitz-consulting.de>
2014-04-22 09:25:01 +0200

    Newton.java hinzugefuegt und Funktionen erstellt, findOneRoot implementiert

--- function/Function.java
+++ function/Function.java
@@ -1,38 +1,111 @@
+
 package function;
 
 import java.util.ArrayList;	
 import java.util.Scanner;
 
 import parser.VPObject;
+import parser.ComponentsManager;
+import scanner.RegexScanner;
 
 public class Function {
 	
-	public String readFunctionFromStdin(){
+	private ComponentsManager componentsManager;
+	private String function;
+	
+	public Function(){
+		throw new IllegalArgumentException("No function provieded!");
+	}
+	
+	public Function(String function){
+		this.function = function;
+		parseComponents(this.function);
+	}
+	
+	public static String readFunctionFromStdin(){
 		Scanner in = new Scanner(System.in);
 		String function = in.next();
 		in.close();
 		return function;
 	}
 	
-	public String derivationFunction(ArrayList<VPObject> vpobjects, ArrayList<String> operations){
-		String diff = "";
-		for(VPObject vpo : vpobjects){
-			diff += operations.get(vpobjects.indexOf(vpo)) + vpo.variableDiff() + "x" + "^" + vpo.potencyDiff();
+	public double value(double x){
+		double functionValue = 0.0;
+		int index = 0;
+		for(VPObject vpo : componentsManager.vpobjects()){
+			index = componentsManager.vpobjects().indexOf(vpo);
+			if(componentsManager.operations().get(index).equals("+")){
+				functionValue += Double.parseDouble(vpo.variable()) * Math.pow(x, Double.parseDouble(vpo.potency()));	
+			}
+			else{
+				functionValue -= Double.parseDouble(vpo.variable()) * Math.pow(x, Double.parseDouble(vpo.potency()));
+			}
 		}
-		return diff;
+		String lastOperation = componentsManager.operations().get(componentsManager.operations().size()-1);
+		if(lastOperation.equals("+")){
+			functionValue += Double.parseDouble(componentsManager.constants());
+		}
+		else{
+			functionValue -= Double.parseDouble(componentsManager.constants());
+		}
+		return functionValue;
 	}
 	
-	public void calculateDerivation(VPObject vpobject){
+	public Function derivation(){
+		String derivation = "";
+		
+		ArrayList<String> operations = componentsManager.operations();
+		ArrayList<VPObject> vpobjects = componentsManager.vpobjects();
+		
+		for(VPObject vpo : vpobjects){
+			String[] derivationVP = calculateDerivation(vpo);
+			derivation += operations.get(vpobjects.indexOf(vpo)) + derivationVP[0] + "x" + "^" + derivationVP[1];
+		}
+		
+		String[] splits = derivation.split("[x]+['^']+[0]");
+		if(splits.length == 1){
+			derivation = splits[0];
+		}
+		
+		splits = derivation.split("[x]+['^']+[1](?![0-9])");
+		String temp = "";
+		for(int i = 0; i < splits.length; i++){
+			temp += splits[i];
+		}
+		derivation = temp;
+		
+		if(derivation.equals("")){
+			derivation = "0";
+		}
+		
+		return new Function(derivation);
+	}
+
+	private void parseComponents(String function){
+		//Funktion => Scanner
+		RegexScanner scanner = new RegexScanner();
+		ArrayList<String> matches = null;
+		matches = scanner.matchComponents(function);
+		
+		//Scanner matches => VPObject + Operationen
+		componentsManager = new ComponentsManager(matches);
+	}
+	
+	private String[] calculateDerivation(VPObject vpobject){
+		String[] derivationVP = new String[2];
 		double variable = Double.parseDouble(vpobject.variable());
 		int potency = Integer.parseInt(vpobject.potency());
 		double variableDiff = variable * potency;
 		int potencyDiff =  potency - 1;
-		vpobject.setVariableDiff(String.valueOf(variableDiff));
-		vpobject.setPotencyDiff(String.valueOf(potencyDiff));
+		derivationVP[0] = String.valueOf(variableDiff);
+		derivationVP[1] = String.valueOf(potencyDiff);
+		
+		return derivationVP;
 	}
 	
-	public double functionValue(String function, double x){
-		return 0.0; //Todo!
+	@Override
+	public String toString()
+	{
+		return function;
 	}
-
 }
